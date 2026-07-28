8dc8d2c6c54217d687cd1b92877481695c99ef4d
Minimal System- Manuale di Gioco <mauromameli.architetto@gmail.com>
2026-07-28 22:38:05 +0200

    Aggiunge web app (Vite+React) con banco di prova del motore del mondo

---
+++ apps/web/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import WorldTester from "./WorldTester.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <WorldTester />
+  </React.StrictMode>
+);
